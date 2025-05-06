9bd7340bf351567648b947ba342b6433c23d26d8
Arshelle9912 <arshelleraheja@gmail.com>
2025-05-06 12:34:07 +0800

    Another question solved

--- Leetcode_287_Duplicate_Number.cpp
+++ Leetcode_287_Duplicate_Number.cpp
@@ -12,5 +12,12 @@ int main() {
     findDuplicate(nums);
 }
 int findDuplicate(vector<int>& nums) {
-        
+    unordered_set<int> set;
+    for (int i = 0; i<nums.size(); i++) {
+        if(set.find(nums[i]) != set.end()) {
+            return nums[i];
+        } else {
+            set.insert(nums[i]);
+        }
+    }
 }
