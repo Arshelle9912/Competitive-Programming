7a70c1944d11420d2bf9822606d8eaabd5f1b818
Arshelle9912 <arshelleraheja@gmail.com>
2025-05-06 11:43:50 +0800

    Another question solved

---
+++ Leetcode_287_Duplicate_Number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+int main() {
+    vector<int> nums;
+    int n;
+    for (int i = 0; i<n; i++) {
+        cin >> n; 
+        nums[i] = n;
+    }
+    findDuplicate(nums);
+}
+int findDuplicate(vector<int>& nums) {
+        
+}
