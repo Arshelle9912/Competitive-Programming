10cf8069b91c92969ea57ec12313a19c8f6983cb
Arshelle9912 <arshelleraheja@gmail.com>
2025-05-12 15:52:26 +0530

    Changed and Updated the Code of this important concept

--- Monotonic_Stack.cpp
+++ Monotonic_Stack.cpp
@@ -1,38 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void print(deque<int> q, vector<int>&arr) {
-    while (!q.empty()) {
-        cout << "{" << arr[q.front()] << ", " << q.front() << "}";
-        q.pop_front();
-    }
-    cout << "\n";
-}
+vector<int> monotonic_stack(vector<int>&, int);
 int main() {
     int n, k;
+    vector<int> ans;
     cin >> n >> k;
     vector<int> a(n);
     for (auto &i:a) {
         cin >> i;
     }
-    deque<int> q;
-    for (int j = 0; j<k; j++) {
-        while (q.size() && a[q.back()] > a[j]) {
-            q.pop_back();
-        }
-        q.push_back(j);
-        cout << j << "\n";
-        print(q, a);
-    } 
-    for (int i = k; i<n; i++) {
-        while (q.size() && a[q.back()] > a[i]) {
-            q.pop_back();
-        }
-        q.push_back(i);
-        if (q.front() == i-k) {
-            q.pop_front();
-        }
-        cout << i << "\n";
-        print(q, a);
+    ans = monotonic_stack(a, k);
+    for (auto x : ans) {
+        cout << x << " ";
     }
+    cout << "\n";
+}
+
+vector<int> monotonic_stack(vector<int> &a, int k) {
+    int n = a.size();
+    vector<int> ans;
+    deque<int> deq;
+    for (int i = 0; i<n; i++) {
+        if (!deq.empty() && deq.front() == i - k) {
+            deq.pop_front();
+        }
+        while (!deq.empty() && a[deq.back()] > a[i]) {
+            deq.pop_back();
+        }
+        deq.push_back(i);
+        
+        if (i >= k-1) {
+            ans.push_back(deq.front() + 1);
+        }
+    }
+    return ans;
 }
