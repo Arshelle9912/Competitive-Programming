8d764a0f0bdb9e73fa7188c93dd3c2f743431635
Arshelle9912 <arshelleraheja@gmail.com>
2025-05-29 03:25:54 +0530

    Leetcode Mediums are easy

--- Greedy/Leetcode_2007.cpp
+++ Greedy/Leetcode_2007.cpp
@@ -13,27 +13,23 @@ vector<int> findOriginalArray(vector<int>& changed) {
     }
     sort(changed.begin(), changed.end());
     for (int i = 0; i<n; i++) {
-        if (changed[i]==0) {
+        if (mp[changed[i]]==0) {
             continue;
         }
-        if (mp[changed[i]]!=0) {
-            mp[changed[i]]--;
-            if (mp[changed[i]]==0) {
-                mp.erase(changed[i]);
-            }
-            if (mp[2*changed[i]]!=0) {
-                result.push_back(changed[i]);
-                mp[2*changed[i]]--;
-                if (mp[2*changed[i]]==0) {
-                    mp.erase(2*changed[i]);
-                }
-            }
+        if (mp[2*changed[i]]==0) {
+            return vector<int>();
+        }
+        mp[changed[i]]--;
+        if (mp[changed[i]]==0) {
+            mp.erase(changed[i]);
+        }
+        result.push_back(changed[i]);
+        mp[2*changed[i]]--;
+        if (mp[2*changed[i]]==0) {
+            mp.erase(2*changed[i]);
         }
     }
-    if (result.size()==n/2) {
-        return result;
-    }
-    return vector<int>();
+    return result;
 }
 
 int main() {
