f28bd0926c619daad698904095147c3ddb211bda
Arshelle9912 <arshelleraheja@gmail.com>
2025-07-05 05:58:03 +0530

    Hard to identify the logic

---
+++ Practice/2109C1.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int target;
+        cin >> target;
+        int temp;
+        cout << "digit" << endl;
+        cin >> temp;
+        cout << "mul 99" << endl;
+        cin >> temp;
+        cout << "digit" << endl;
+        cin >> temp;
+        cout << "add " << target-18 << endl;
+        cin >> temp;
+        cout << "!" << endl;
+        cin >> temp;
+    }
+}
