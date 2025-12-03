e0d3dee6ccf2f5bf108ec93d0a1b2c125c8fd476
Arshelle9912 <arshelleraheja@gmail.com>
2025-12-03 11:12:49 +0800

    Easy div2 A

---
+++ Practice/2158A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, y, r;
+        cin >> n;
+        cin >> y >> r;
+        cout << min(n, r+y/2) << "\n";
+    }
+}
